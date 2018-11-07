f8a9836475d88425e82c673a1dea8d8aec8c2665
Prianichnikov <prianik@w2k.biochem.mpg.de>
2018-11-07 17:20:32 +0100

    Initial commit

---
+++ FUrl/Options.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using CommandLine;
+using CommandLine.Text;
+
+namespace FUrl
+{
+// Define a class to receive parsed values
+    public enum Method
+    {
+        Get, Post, Put, Delete, Patch
+    }
+
+    
+    class Options {
+        [Value(0, MetaName = "url", Required = true, HelpText = "Url")]
+        public string Url { get; set; }
+    
+        [Option('f', "form", HelpText = "Form datas")]
+        public IEnumerable<string> FormData { get; set; }
+        
+        [Option('h', "headers", HelpText = "Headers")]
+        public IEnumerable<string> Headers { get; set; }
+
+        private static (string, string) SplitOrThrow(string value, string delimiter = "=")
+        {
+            var tokens = value.Split(delimiter);
+            if (tokens.Length != 2)
+            {
+                throw new Exception($"Invalid format: {value}");
+            }
+
+            return (tokens[0], tokens[1]);
+        }
+
+//        [Option('q', "query-param", HelpText = "Query params")]
+//        public  IEnumerable<string> QueryParam { get; set; }
+
+        [Option('m', "method", HelpText = "HTTP Method", Default = Method.Get)]
+        public Method Method { get; set; }
+            
+//        [Option('v', "verbose", Default = true,
+//            HelpText = "Prints all messages to standard output.")]
+//        public bool Verbose { get; set; }
+        
+        public IEnumerable<(string, string)> HeadersParsed
+        {
+            get { return Headers.Select(x => SplitOrThrow(x, ":")); }
+        }
+        
+//        public IEnumerable<(string, string)> QueryParamParsed
+//        {
+//            get { return QueryParam.Select(x => SplitOrThrow(x)); }
+//        }
+        
+        public IEnumerable<(string, string)> FormDataParsed
+        {
+            get { return FormData.Select(x => SplitOrThrow(x)); }
+        }
+    }
+}
---
+++ FUrl/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+
+namespace FUrl
+{
+  class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var result =  Parser.Default.ParseArguments<Options>(args);
+            await result.MapResult(
+                async opts => await RunOptionsAndReturnExitCode(opts),
+                errors => Task.FromResult(0)
+            );
+//                .WithParsed(RunOptionsAndReturnExitCode)
+//                .WithNotParsed(HandleParseError);
+           
+//            
+//            var assetId = "5"; //args[0];
+//            var version = "xyz"; //args[1];
+//            var assetFileName = "/Users/nikita/Sources/python/maxquant_download/manage.py"; // args[2];
+//            var baseUrl = "http://127.0.0.1:8000/api/assetversions/";
+//            var token = "573e199074c94184a7dc3799b054166af6cd6bae";
+//            var headers = new Dictionary<string, string>
+//            {
+//                {"Authorization", $"Token {token}"},
+//                {"Accept" , "*/*"},
+//            };
+//
+//            var parameters = new Dictionary<string, string>()
+//            {
+//                {"asset", assetId},
+//                {"version", version}
+//            };
+//            using (var fs = File.OpenRead(assetFileName))
+//            {
+//                var files = new Dictionary<string, (string, FileStream)>()
+//                {
+//                    {"file", (Path.GetFileName(assetFileName), fs)}
+//                };
+//                var res = await Upload(baseUrl, headers, parameters, files);
+//                using (StreamReader reader = new StreamReader(res, Encoding.UTF8))
+//                {
+//                    var data = await reader.ReadToEndAsync();
+//                    Console.WriteLine(data);
+//                }
+//
+//                
+//            } 
+           
+        }
+
+        private static void HandleParseError(IEnumerable<Error> errs)
+        {
+//            Console.WriteLine(errs);
+        }
+
+        private static async Task<int> RunOptionsAndReturnExitCode(Options opts)
+        {
+            Console.WriteLine(opts.Method);
+            var res = await Upload(ParseOptions(opts));
+            using (var reader = new StreamReader(res, Encoding.UTF8))
+            {
+                var data = await reader.ReadToEndAsync();
+                Console.WriteLine(data);
+            }
+
+            return 0;
+        }
+
+        private static HttpRequestMessage ParseOptions(Options options)
+        {
+            var method = new HttpMethod(options.Method.ToString());
+            var request = new HttpRequestMessage(method, options.Url);
+            
+            var formData = new MultipartFormDataContent();
+            foreach (var (name, value) in options.HeadersParsed)
+            {
+                request.Headers.TryAddWithoutValidation(name, value);
+            }
+
+            foreach (var (name, value) in options.FormDataParsed)
+            {
+                if (value.StartsWith("@"))
+                {
+                    var assetFileName = value.TrimStart('@');
+                    var fs = File.OpenRead(assetFileName);
+                    var baseName = Path.GetFileName(assetFileName);
+                    formData.Add(new StreamContent(fs), name, baseName);
+                }
+                else
+                {
+                    formData.Add(new StringContent(name), value);    
+                }
+                
+            }
+            request.Content = formData;
+            return request;
+        }
+        
+        private static async Task<Stream> Upload(HttpRequestMessage request)
+        {
+            using (var client = new HttpClient())
+            {
+                var response = await client.SendAsync(request);
+                return await response.Content.ReadAsStreamAsync();
+            }
+        }
+    }
+}
